a13acc470ecac98a380375029e7a6f05be87b76b
Jay <jjrg1994@gmail.com>
2016-08-19 22:29:56 -0500

    Auth

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
 var port = process.env.PORT || 8080;
 var app = express();
 var User = require('./models/user');
+var secret = 'dampenHarm4355chiorbIt';
 
 //config
 
@@ -29,13 +31,83 @@ app.get('/', function(req, res){
 	res.send('Home site for bikes4free');
 });
 
+
 //API
 var apiRouter = express.Router();
 
+/**
+ * Token provider
+ */
+apiRouter.post('/authenticate', function(req, res){
+	User.findOne({
+		username: req.body.username
+	}).select('name username password').exec(function(err, user){
+		if(err) throw err;
+
+		if(!user){
+			res.json({
+				success: false,
+				message: 'Auth failed: user not found'
+			});
+		} else if (user){
+			var validPassword = user.comparePassword(req.body.password);
+			if(!validPassword){
+				res.json({
+					success: false,
+					message: 'Auth failed: incorrect password'
+				});
+			}else {
+				//JOT or JWT structure
+				var token = jwt.sign({
+					name: user.name,
+					username: user.username
+				}, secret, {
+					expiresIn: 60*24 //1dia
+				});
+
+				res.json({
+					success: true, 
+					message: 'token generated',
+					token: token
+				});
+			}
+		}
+	});
+});
+
+//Middleware
 apiRouter.use(function(req, res, next){
-	console.log('Website hit');
-	//middleware stuff
-	next();
+	//recibir token via:
+	// query string param, form body param o http header
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	if(token){
+		jwt.verify(token, secret, function(err, decoded){
+			if (err){
+				return res.status(403).send({
+					success: false,
+					message: 'Token verification failed'
+				});
+			} else {
+				req.decoded = decoded;
+				next();
+			}
+		});
+	} else {
+		return res.status(403).send({
+			success: false,
+			message: 'Please provide a token'
+		});
+	}
+
+});
+
+/**
+ * Send logged user
+ * @return {[type]}                                 [description]
+ */
+apiRouter.get('/me', function(req, res){
+	res.send(req.decoded);
 });
 
 /**
@@ -45,8 +117,8 @@ apiRouter.use(function(req, res, next){
 apiRouter.route('/users')
 	//CREATION
 	.post(function(req, res){
-		var user = new User();
-		user.name = req.body.name;
+		var user = new User()
+;		user.name = req.body.name;
 		user.username = req.body.username;
 		user.password = req.body.password;
 		user.save(function(err){
